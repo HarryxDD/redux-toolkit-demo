65c5a4e4a6a8d9a7891626ae22b71b452ca6dd87
Harry <harrytruong1772@gmail.com>
2022-11-01 00:49:55 +0700

    first push

---
+++ src/redux/actions.js
@@ -0,0 +1,39 @@
+// export const addTodoAction = {
+//     type: 'todoList/addTodo',
+//     payload: { id: 5, name: 'Learn NodeJS', completed: false, priority: 'High' }
+// }
+
+export const addTodo = (data) => {
+    return {
+        type: 'todoList/addTodo',
+        payload: data
+    }
+}
+
+export const toggleTodoStatus = (todoId) => {
+    return {
+        type: 'todoList/toggleTodoStatus',
+        payload: todoId,
+    }
+}
+
+export const searchFilterChange = (text) => {
+    return {
+        type: 'filters/searchFilterChange',
+        payload: text
+    }
+}
+
+export const statusFilterChange = (status) => {
+    return {
+        type: 'filters/statusFilterChange',
+        payload: status
+    }
+}
+
+export const priorityFilterChange = (priorities) => {
+    return {
+        type: 'filters/prioritiesFilterChange',
+        payload: priorities
+    }
+}
